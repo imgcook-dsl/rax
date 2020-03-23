a85e387f3811d3f6445f8047e633b3b008f70c30
gindis <flyxiang228@gmail.com>
2020-03-23 17:04:04 +0800

    fix

--- src/exportPage.js
+++ src/exportPage.js
@@ -50,7 +50,7 @@ function exportPage(schema, option) {
   const collectImports = componentName => {
     let componentMap = componentsMap[componentName] || {};
     let packageName = componentMap.packageName || componentName;
-    if (packageName && ['view', 'image', 'text'].indexOf(packageName.toLowerCase()) > 0) {
+    if (packageName && ['view', 'image', 'text'].indexOf(packageName.toLowerCase()) >= 0) {
       packageName = `rax-${packageName.toLowerCase()}`;
     }
     const singleImport = `import ${componentName} from '${packageName}'`;
